730a7ab7e32d5e37375a53f8812ec3f78c7acd41
Yichen Liu <jp2019213039@qmul.ac.uk>
2022-05-21 18:09:12 +0800

    1

--- Structure/structure.c
+++ Structure/structure.c
@@ -1,12 +1,34 @@
 //
 // Created by 16003 on 2022/5/19.
 //
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
-#include<string.h>
-#include <time.h>
+#include <stdio.h> /* for printf() and fprintf() */
+#include <sys/socket.h> /* for socket(), sendto() and recvfrom() */
+#include <arpa/inet.h> /* for sockaddr_in and inet_addr() */
+#include <stdlib.h> /* for atoi() and exit() */
+#include <string.h> /* for memset() */
+#include <unistd.h> /* for close() */
+#include <netdb.h>
+#include <fcntl.h>
 
+#define FILE_MODE 0777
+#define IP_LEN	64
+int fd;
+int sock;
+
+struct sockaddr_in serverSockAddr; /* server address */
+struct sockaddr_in clientSockAddr; /* client address */
+
+char sendBuff[20000];
+char rcvBuff[20000];
+char fileBuff[20000];
+
+char servIP[64];
+char servDomainName[64];
+
+int rcvLength;
+int sendLength;
+
+const unsigned short servPort = 110;
 
 struct Email{
     char subject[50];
@@ -16,22 +38,59 @@ struct Email{
 void Displaymainmenu();//显示主菜单
 int getList();//Get a list of messages and sizes  success:0,fail:-1
 int getMailStatus();//Get mail status             success:0,fail:-1
-int displayDetail();//Display mail in detail      success:0,fail:-1
+int getMailDetail(char *);//Display mail in detail      success:0,fail:-1
 int search();//Search text in all mails           success:0,fail:-1
 int displayBySubjects();//Display by subjects     success:0,fail:-1
-int downloadDelete();//Download the mail and delete in the remote service          success:0,fail:-1
+int downloadDelete(char *);//Download the mail and delete in the remote service          success:0,fail:-1
 int quit();//Quit                                 success:0,fail:-1
 void nextStep();//show next step
 int login();//get in the pop.163.com              success:0,fail:-1
+int getIpFormDomainName(char* , char* , unsigned char);
+int getIpFormDomainName(char* , char* , unsigned char);
+int sendToServer(char *);
+int rcvFromServer();
+int isSuccessful(char*);
+int anythingRecieved(int);
+int sendReceive(char* );
+int reset();
+int readFlie(char* );
+int connectToServer();
 
 
 int main()
 {
     //TCP链接
-
+    connectToServer();
+    rcvFromServer();
+    printf("%s",rcvBuff);
+    memset(rcvBuff,0,sizeof(rcvBuff));
     //login
-    if(login() < 0)
-        printf("login() fail.\n");
+    switch (login())
+    {
+        case 0:
+            printf("nothing returned from server\n");
+            break;
+
+        case -1:
+            printf("receive error\n");
+            break;
+
+        case 1:
+            printf("successfully login!\n");
+            printf("%s",rcvBuff);
+            break;
+
+        case 2:
+            printf("return error\n");
+            break;
+
+        default:
+            break;
+    }
+    memset(rcvBuff,0,sizeof(rcvBuff));
+
+
+
     //Display main menu
 
     Displaymainmenu();
@@ -71,7 +130,7 @@ void Displaymainmenu()//显示主菜单
             }
 
             case 3:{
-                if(displayDetail() < 0)
+                if(getMailDetail() < 0)
                     printf("displayDetail() fail.\n");
                 break;
             }
@@ -136,18 +195,126 @@ void nextStep(){
     }
 }
 
+int getIpFormDomainName(char* servDomainName, char* servIP, unsigned char ipLen)
+{
+    struct hostent* host = NULL;
+    host = gethostbyname(servDomainName);
+
+    if(NULL == host)
+    {
+        return -1;
+    }
+    inet_ntop(host->h_addrtype, host->h_addr, servIP, ipLen);
+
+    return 0;
+}
+
+int connectToServer(){//成功返回0，失败返-1
+    printf("Enter the domain name you want to connect:\n");
+    scanf("%s",servDomainName);
+
+
+    if(getIpFormDomainName(servDomainName,servIP,IP_LEN)==-1){
+        printf("Fail to get IP address from domain name\n");
+        return -1;
+    }
+
+
+    if ((sock = socket(PF_INET, SOCK_STREAM, 0)) < 0){
+        printf("socket() failed.\n");
+        return -1;
+    }
+
+    memset(&serverSockAddr, 0, sizeof(serverSockAddr));   /*Zero out structure*/
+    serverSockAddr.sin_family = AF_INET;                /* Internet addr family */
+    serverSockAddr.sin_addr.s_addr = inet_addr(servIP); //IP address in numbers-and-dots notation (ASCII string) to IP address structure in network byte order
+    serverSockAddr.sin_port = htons(servPort);      /* Server port */
+
+    if(connect(sock,(struct sockaddr *) &serverSockAddr,sizeof(serverSockAddr))<0){
+        printf("connecting to %s\n",servIP);
+        printf("connect failed.\n");
+        return -1;
+    }
+    else
+        printf("Connect to server successfully!\n");
+    return 1;
+}
+
+int sendToServer(char *message){
+    //sendBuff = message;
+    if(send(sock,message,strlen(message),0)==-1){
+        return -1;
+    }
+    return 0;
+
+}
+
+int rcvFromServer(){
+    return  recv(sock,rcvBuff,sizeof (rcvBuff),0);
+
+}
+
+int isSuccessful(char* str){
+    char* str2 = "OK";
+    if(strstr(str, str2)==NULL)
+        return -1;
+    else
+        return 0;
+}
+
+int anythingRecieved(int charNum){////-1:receive error   0:no received message    1:normal
+    if(charNum == -1){
+        return -1;
+    }
+    if(charNum == 0){
+        return 0;
+    }
+    return 1;
+}
+
+int sendReceive(char* str){//return:-1:receive error   0:receive nothing   1:success   2:return -ERR
+    sendToServer(str);
+    int i = rcvFromServer();
+    if(anythingRecieved(i) <=0)
+        return anythingRecieved(i);
+    else
+    {
+        if(isSuccessful(rcvBuff)==0)
+            return 1;
+        else
+            return 2;
+    }
+}
+
 int quit(){
-    printf("This is quite.\n");
+    sendReceive("QUIT\n");
     return 0;
 }
 
 int login(){
-    printf("This is login.\n");
+    sendReceive("USER IA_pop3_client\n");
+    memset(rcvBuff,0,sizeof(rcvBuff));
+    sendReceive("PASS NHRIWKBTQGCWGIUB\n");
     return 0;
 }
 
-int downloadDelete(){
-    printf("This is downloadDelete.\n");
+int downloadDelete(char *i){
+    int v = getMailDetail(i);
+
+    char str[100]="DELE ";
+    strcat(str, i);
+
+    if(v!=1)
+        return v;
+    else{
+        FILE *fp;
+        fp = fopen("test.eml","w+");///////the name here must match the mail user selects
+        fprintf(fp,"%s",rcvBuff);
+        fclose(fp);
+
+        memset(rcvBuff,0,sizeof(rcvBuff));
+        sendReceive(str);
+    }
     return 0;
 }
 
@@ -161,17 +328,32 @@ int search(){
     return 0;
 }
 
-int displayDetail(){
-    printf("This is displayDetail.\n");
+int getMailDetail(char *i){
+    char str[100]="RETR ";
+    strcat(str, i);
+    sendReceive(str);
     return 0;
 }
 
 int getMailStatus(){
-    printf("This is getMailStatus.\n");
+    sendReceive("STAT\n");
     return 0;
 }
 
 int getList(){
-    printf("This is getList.\n");
+    sendReceive("LIST\n");
     return 0;
+}
+
+int reset(){
+    sendReceive("RSET\n");
+}
+
+int readFlie(char* fliename){
+    if((fd = open(fliename,O_RDONLY,0))==-1)
+        return -1;
+
+    int i = read(fd,fileBuff,sizeof(fileBuff));
+    close(fd);
+    return i;
 }
