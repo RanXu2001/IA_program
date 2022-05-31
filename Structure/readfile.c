7920a53b8f29f4b1c521ad1d5c0e6df2690549ee
Yichen Liu <jp2019213039@qmul.ac.uk>
2022-05-31 18:24:09 +0800

    service失败则直接推出

--- Structure/readfile.c
+++ Structure/readfile.c
@@ -2,55 +2,41 @@
 // Created by 16003 on 2022/5/31.
 //
 #include <stdio.h> /* for printf() and fprintf() */
-
 #include <stdlib.h> /* for atoi() and exit() */
 #include <string.h> /* for memset() */
 #include <unistd.h> /* for close() */
 #include <fcntl.h>
-int fd;
+#include <dirent.h>
+FILE *f1;
 char fileBuff[20000];
+char subjectName[20000][20000];
 
-typedef struct split_str_struct
-{
-    char *str;
-    unsigned short length;
-}SPLITSTRUCT;
-
-static  unsigned short StringSplit(char *instr, char *token, SPLITSTRUCT *out_strs)
-{
-    char *start = instr;
-    char *end = NULL;
-    int ind = 0;
-
-    if(!instr || !instr[0])
+int findsubject(char* fliename,char value[1024]){
+    int len;
+    char subject[1024]="Subject: ";
+    strcat(subject,value);
+    char *tsub;
+    if((f1 = fopen(fliename,"r"))== NULL)
         return -1;
 
-    end = strstr(start, token);
-    while(end != NULL) {
-        out_strs[ind].str = start;
-        out_strs[ind].length = end - start;
-
-        ind ++;
-        start = end + strlen(token);
-        end = strstr(start, token);
+    while(fgets(fileBuff,20000,f1) != NULL)
+    {
+        len = strlen(fileBuff);
+        fileBuff[len-1] = '\0';  /*去掉换行符*/
+        if(strcmp(fileBuff,subject) == 0){
+            //tsub = &fileBuff[9];
+            //printf("%s\n",tsub);
+            //char *data = strstr(fileBuff,subject);
+            //printf("%s\n",data+9);
+            printf("find it!");
+        }
+        //printf("%s %d \n",fileBuff,len - 1);
     }
-    out_strs[ind].str = start;
-    out_strs[ind].length = strlen(start);
-
-    return ind + 1;
-}
-
-int readFlie(char* fliename){
-    if((fd = open(fliename,O_RDONLY,0))==-1)
-        return -1;
-
-    int i = read(fd,fileBuff,sizeof(fileBuff));
-    close(fd);
-    return i;
+    return 0;
 }
 
 int main(){
-    int v= readFlie("em1.eml");
-    printf("%s",fileBuff);
+    int v= findsubject("em2.eml","TEST 1");
+    printf(v);
     return 0;
 }
